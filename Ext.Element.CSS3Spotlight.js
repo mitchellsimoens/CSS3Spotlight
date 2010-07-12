a5feaf6a650df4b565281f506ee56ebeec30ab64
Mitchell Simoens <ms.simoens@gmail.com>
2010-07-12 11:42:54 -0500

    Added successive click protection and some abstraction

--- Ext.Element.CSS3Spotlight.js
+++ Ext.Element.CSS3Spotlight.js
@@ -5,62 +5,106 @@ Ext.Element.addMethods({
 	        duration : 500,
 	        easing   : "ease-in"
 	    });
-		var spot = Ext.DomHelper.append(Ext.getBody().dom, {
-			tag: "div",
-		}, true);
+		if (this.spotlight) {
+			if (this.spotlight.active === true) {
+				return;
+			}
+		} else {
+			this.spotlight = Ext.DomHelper.append(Ext.getBody().dom, {
+				tag: "div"
+			}, true);
+		}
+		this.spotlight.active = true;
 		
-		this.spotlight = spot;
-		
-		var duration = (o.skipAnim === true) ? 0 : o.duration;
-		
-		spot.setStyle({
+		this.spotlight.setStyle({
 			"background-color" : "#000",
 			"position" : "absolute",
 			"top" : "0px",
 			"left" : "0px",
 			"width" : "100%",
 			"height" : "100%",
-			"z-index" : this.zindex-1,
-			"opacity" : "0"
+			"z-index" : this.zindex-1
 		});
-		new Ext.util.DelayedTask(function() {
-			spot.setStyle({
-				"opacity" : "0.5",
-				"-webkit-transform": "opacity",
-				"-webkit-transition-timing-function": o.easing,
-				"-webkit-transition-duration": duration+"ms",
-				"-moz-transform": "opacity",
-				"-moz-transition-timing-function": o.easing,
-				"-moz-transition-duration": duration+"ms",
-				"-o-transform": "opacity",
-				"-o-transition-timing-function": o.easing,
-				"-o-transition-duration": duration+"ms"
-			});
-		}).delay(100);
+		
+		if (o.skipAnim === true) {
+			this.spotlight.setStyle("opacity", "0.5");
+			this.CSS3SpotlightMarkShowDone();
+		} else {
+			this.spotlight.setStyle("opacity", "0");
+			this.CSS3SpotlightShow(o);
+		}
 	},
-	CSS3SpotlightHide : function(o) {
+	CSS3SpotlightMarkShowDone : function() {
+		this.spotlight.active = false;
+	},
+	CSS3SpotlightMarkHideDone : function() {
+		this.spotlight.active = false;
+		delete this.spotlight;
+		return;
+	},
+	CSS3SpotlightShow : function (o) {
 		o = Ext.applyIf(o || {}, {
 			skipAnim : false,
 	        duration : 500,
-	        easing   : "ease-out"
+	        easing   : "ease-out",
+	        opacity  : 0.5,
+	        callback : Ext.emptyFn
+	    });
+		new Ext.util.DelayedTask(function() {
+			o.opactiy = 0.5;
+			o.callback = this.CSS3SpotlightMarkShowDone;
+			this.CSS3SpotlightStartAnim(o);
+		}, this).delay(100);
+	},
+	CSS3SpotlightHide : function(o) {
+		this.spotlight.active = true;
+		o = Ext.applyIf(o || {}, {
+			skipAnim : false,
+	        duration : 500,
+	        easing   : "ease-out",
+	        opacity  : 0,
+	        callback : Ext.emptyFn
 	    });
 		
-		var duration = (o.skipAnim === true) ? 0 : o.duration;
-		
+		if (o.skipAnim === true) {
+			Ext.removeNode(this.spotlight.dom);
+			this.CSS3SpotlightMarkHideDone();
+			return;
+		}
+
+		o.opactiy = 0;
+		o.callback = this.CSS3SpotlightMarkHideDone;
+		this.CSS3SpotlightStartAnim(o);
+	},
+	CSS3SpotlightStartAnim : function(o) {
+		o = Ext.applyIf(o || {}, {
+	        duration : 500,
+	        easing   : "ease-out"
+	    });
 		this.spotlight.setStyle({
-			"opacity" : "0",
+			"opacity" : o.opacity,
 			"-webkit-transform": "opacity",
 			"-webkit-transition-timing-function": o.easing,
-			"-webkit-transition-duration": duration+"ms",
+			"-webkit-transition-duration": o.duration+"ms",
 			"-moz-transform": "opacity",
 			"-moz-transition-timing-function": o.easing,
-			"-moz-transition-duration": duration+"ms",
+			"-moz-transition-duration": o.duration+"ms",
 			"-o-transform": "opacity",
 			"-o-transition-timing-function": o.easing,
-			"-o-transition-duration": duration+"ms"
+			"-o-transition-duration": o.duration+"ms"
 		});
-		new Ext.util.DelayedTask(function() {
-			Ext.removeNode(this.spotlight.dom);
-		}, this).delay(duration);
+		if (Ext.isWebKit) {
+			this.spotlight.addListener("webkitTransitionEnd", o.callback, this, {
+				single: true
+			});
+		} else if (Ext.isGecko4) {
+			this.spotlight.addListener("transitionend", o.callback, this, {
+				single: true
+			});
+		} else if (Ext.isOpera) {
+			this.spotlight.addListener("oTransitionEnd", o.callback, this, {
+				single: true
+			});
+		}
 	}
 });
